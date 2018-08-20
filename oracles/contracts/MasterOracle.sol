f3c3bf2476f3dfc174a2de5467fcf198cd5159c0
Anatoly <sindbag@gmail.com>
2018-08-20 18:59:51 +0300

    Update MasterOracle.sol

--- oracles/contracts/MasterOracle.sol
+++ oracles/contracts/MasterOracle.sol
@@ -2,8 +2,12 @@ pragma solidity ^0.4.24;
 import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 
 contract MasterOracle is Ownable {
-    event DataRequest(string name, address receiver);
-    function request_data(string name, address receiver) public {
-        emit DataRequest(name, receiver);
+    event DataRequest(string name, address receiver, string memo);
+    event DataRequest(string name, address receiver, string memo, int[] params);
+    function request_data(string name, address receiver, string memo) public {
+        emit DataRequest(name, receiver, memo);
+    }
+    function request_data_args(string name, address receiver, string memo, int[] params) public {
+        emit DataRequest(name, receiver, memo, params);
     }
 }
