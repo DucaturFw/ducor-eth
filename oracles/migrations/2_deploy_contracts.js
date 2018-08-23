ada92cf1ad2262a4930152d4ef814d0519bfde67
Anatoly <sindbag@gmail.com>
2018-08-23 19:03:05 +0300

    Update 2_deploy_contracts.js

--- oracles/migrations/2_deploy_contracts.js
+++ oracles/migrations/2_deploy_contracts.js
@@ -5,6 +5,7 @@ var MTDataConsumer = artifacts.require("./MultiTypeDataConsumer.sol");
 
 module.exports = function(deployer) {
     deployer.deploy(MasterOracle).then(() => {
+        console.log('MASTER ORACLE ADDRESS: ', MasterOracle.address)
         deployer.deploy(DataConsumer,
             '0x0abc123', 10, 10 * Math.pow(10, 10),
             MasterOracle.address, 0x0, 100, 100000);
